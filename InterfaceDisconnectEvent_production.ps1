843c1be5a508f20909d6e0f001eefc9020cb01d6
Tom <tomtcvanpelt@gmail.com>
2022-11-22 10:24:03 +0100

    changed time window

--- InterfaceDisconnectEvent_production.ps1
+++ InterfaceDisconnectEvent_production.ps1
@@ -1,8 +1,8 @@
 #Copyright 2022 - Tom van Pelt
 
-#Get Last Interface Adapter Disconnect Event from last 24 hours
 try {
-    $lastIfDiscEvent = Get-WinEvent -MaxEvents 1 -FilterHashtable @{ #get 1 disconnect event
+    #Get Last Interface Adapter Disconnect Event from last 24 hours
+    $lastIfDiscEvent = Get-WinEvent -MaxEvents 1 -FilterHashtable @{
     LogName = "System";
     ID = 27;
     Level = 3; #warning
@@ -25,17 +25,11 @@ $lastBootUptime = (Get-CimInstance -ClassName win32_operatingsystem).LastBootUpT
 Write-Host "Last Ethernet Disconnect Event Time: $lastIfDiscEventTime";
 Write-Host "Last Boot Uptime: $lastBootUptime";
 
-#Test Prints:
-#Write-Host "Last Ethernet Disconnect Event Time Plus 1 Minute: $lastEthDisconnectEventPlus"
-#Write-Host "Last Ethernet Disconnect Event Time Min 1 Minute: $lastEthDisconnectEventMin"
-
 $isReboot = "True" #0 if a reboot or startup, 1 if not a reboot or startup
 if (($lastBootUptime -gt $lastIfDiscEventTimeMin) -and ($lastBootUptime -lt $lastIfDiscEventTimePlus)) {
-    #last ethernet disconnect event was a reboot (or startup)
     Write-Host "Disconnect event was a reboot or Startup.";
 }
 elseif (($lastIfDiscEventTimePlus.addSeconds(1)) -gt $lastBootUptime) {
-    #failed
     Write-Host "Interface is Disconnected.";
     $isReboot = "False"; #check failed is true
 }
